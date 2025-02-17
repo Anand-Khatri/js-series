10dc142dbfdaa936996305528856548dbd790159
Anand-Khatri <144843827+Anand-Khatri@users.noreply.github.com>
2025-02-17 18:30:55 +0000

    convertion of datatype

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score="anand"
+
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+
+// "33" => 33
+ // "33abc" => NAN
+ // true => 1 ;  false=> 0
+
+ let isLoggedIn="1"
+
+ let BooleanIsLoggedIn=Boolean(isLoggedIn)
+
+ console.log(BooleanIsLoggedIn)
+
+ //"1"=> true ;  "0"=>false
+//""=>false   or "anand"=>true
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(stringnumber)
+
+console.log(typeof stringnumber)
+
+
+
+
