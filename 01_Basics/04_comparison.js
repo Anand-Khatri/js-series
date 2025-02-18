5c81d6dd1af3f8ea5c86f1739ddd1f33a3f904b5
Anand-Khatri <144843827+Anand-Khatri@users.noreply.github.com>
2025-02-18 09:09:34 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,22 @@
+console.log(4>2)
+
+console.log("2">1)
+console.log("02">1)
+
+
+console.log(null>0)  //o/p=> false
+console.log(null==0)  // o/p => false
+console.log(null>=0)  // null treated as zero that why give o/p=> true 
+
+
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined<0)
+
+
+
+//===
+
+console.log("2"=="2")
+
