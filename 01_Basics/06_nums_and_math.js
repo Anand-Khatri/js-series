406d6c88a2facb8f58cbfc1bd853b62d55235f71
Anand-Khatri <144843827+Anand-Khatri@users.noreply.github.com>
2025-02-21 08:02:52 +0000

    Math and num

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,44 @@
+const score=400
+
+//console.log(score)
+
+const balance=new Number(400)
+
+//console.log(balance)
+
+//console.log(balance.toString().length)
+
+//console.log(balance.toFixed(1))
+
+//console.log(typeof balance)
+
+const anothernumber=102.5624
+//console.log(anothernumber.toPrecision(3))
+
+
+const hundreds=100000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++  Math ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+//console.log(Math)
+//console.log(Math.abs(-4))
+//console.log(Math.round(4.7))
+//console.log(Math.ceil(4.3))
+//console.log(Math.floor(4.7))
+//console.log(Math.min(4,5,2,3))
+//console.log(Math.max(5,4,8,7))
+
+console.log(Math.floor(Math.random()*10))
+console.log(Math.floor(Math.random()*10)+1)
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)) +min )
+
+
+
+
